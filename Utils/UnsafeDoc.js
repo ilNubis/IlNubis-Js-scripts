0297d20884b91594860884f12e6e2ab67d5e3582
Valerio <valerio.panebianco.04@gmail.com>
2026-01-08 22:28:56 +0100

    feat: Added `Utils\UnsafeDoc.js`

---
+++ Utils/UnsafeDoc.js
@@ -0,0 +1,125 @@
+// depend on https://github.com/ilNubis/IlNubis-Js-scripts/raw/refs/heads/main/Utils/AssertLib.js
+
+
+// -- Dev docs --
+// This set of function allow to grab some HTMLElements with async interval
+// 
+class IntervalDoc {
+
+    static _opzPreset = {
+        intervalMs: 100,
+        maxIntervalCycles: 100
+    };
+
+    static mergeOpz(dest, src) {
+        AssertLib.assertTypeArgs([
+            ["dest", dest, "object"],
+            ["src", src, "object"]
+        ]);
+
+        if (undefined === dest) {
+            dest = {}
+        }
+
+        if (undefined === dest.intervalMS) {
+            dest.intervalMS = src.intervalMS;
+        }
+
+        if (undefined === dest.maxIntervalCycles) {
+            dest.maxIntervalCycles = src.maxIntervalCycles;
+        }
+
+        return dest;
+    }
+
+    static getElementByid (elemId, func, opz) {
+        AssertLib.assertTypeArgs([
+            "IntervalDoc.getElementByid()",
+            ["elemId", elemId, "string"],
+            ["func", func, "function"],
+            ["opz", opz, ["object", undefined]]
+        ]);
+
+        UnsafeDoc.mergeOpz(opz, UnsafeDoc._opzPreset);
+
+        var elem = undefined;
+
+        var intervalCycles = 0;
+        const intervalId = setInterval(() => {    
+            if (intervalCycles >= opz.intervalMs) {
+                clearInterval(intervalId);
+            }
+            elem = document.getElementById(elemId)
+            
+            if (elem) {
+                clearInterval(intervalId);
+                func(elem);
+            }
+            intervalCycles++;
+        }, opz.intervalMs);
+
+        return intervalId;
+    }
+
+    static getElementsByClassName(elemClassName, func, opz) {
+        AssertLib.assertTypeArgs([
+            "IntervalDoc.getElementsByClassName()",
+            ["elemClassName", elemClassName, "string"],
+            ["func", func, "function"],
+            ["opz", opz, ["object", undefined]]
+        ]);
+
+        UnsafeDoc.mergeOpz(opz, UnsafeDoc._opzPreset);
+
+        var elems = undefined;
+
+        var intervalCycles = 0;
+        const intervalId = setInterval(() => {    
+            if (intervalCycles >= opz.intervalMs) {
+                clearInterval(intervalId);
+            }
+            elems = document.getElementsByClassName(elemClassName);
+            
+            if (elems.length > 0) {
+                clearInterval(intervalId);
+                func(elems)
+            }
+            intervalCycles++;
+        }, opz.intervalMs);
+
+        return intervalId;
+    }
+
+
+    static getElementsByTagName(elemTagName, func, opz) {
+        AssertLib.assertTypeArgs([
+            "IntervalDoc.getElementsByTagName()",
+            ["elemTagName", elemTagName, "string"],
+            ["func", func, "function"],
+            ["opz", opz, ["object", undefined]]
+        ]);
+
+        UnsafeDoc.mergeOpz(opz, UnsafeDoc._opzPreset);
+
+        var elems = undefined;
+
+        var intervalCycles = 0;
+        const intervalId = setInterval(() => {    
+            if (intervalCycles >= opz.intervalMs) {
+                clearInterval(intervalId);
+            }
+            elems = document.getElementsByTagName(elemTagName);
+            
+            if (elems.length > 0) {
+                clearInterval(intervalId);
+                func(elems)
+            }
+            intervalCycles++;
+        }, opz.intervalMs);
+
+        return intervalId;
+    }
+
+    // TODO: Other getElements
+
+}
